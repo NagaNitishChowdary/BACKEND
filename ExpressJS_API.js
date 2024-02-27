50aad74252781147b8448031aec1508ef949a21e
Naga Nitish <naganitishkapu@gmail.com>
2024-02-27 19:22:03 +0530

    Update ExpressJS_API.js

--- ExpressJS_API.js
+++ ExpressJS_API.js
@@ -21,6 +21,12 @@ app.use((req,res,next) => {
     res.status(404).send('<h1>Page Not Found In The Server.</h1>') ; 
 });
 
+// OR 
+
+// app.get('*',(req,res) => {
+//     res.status(404).send('<h1>Page Not Found</h1>') ; 
+// })
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`) ; 
 })
